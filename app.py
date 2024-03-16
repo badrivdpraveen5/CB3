38830eb0689076445e8748a9ce72a4d102c3e70e
Badri Venkata Durga Praveen <badrivdpraveen5@gmail.com>
2024-03-16 12:22:09 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4354039f55a5f0b2b6b18a2cc72d0ab7204d5f26eae549f3c869dd48f4f2f16b
+size 1690
---
+++ bone_fracture_detector.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:91bc2655d815de54b4282ad5bedf6f87171a35abb088c11fa8b2fe2599e469f5
+size 987
